a2958fb75c96b37c1dd30ad32061f1b637974cbd
Bailey Kasin <baileykasin@gmail.com>
2018-03-12 19:56:39 -0700

    Bit more work on the scoring engine. Figuring out best way to handle parsing the JSON for what to do

--- ScoringEngine/CyberPatriotWindows/Program.cs
+++ ScoringEngine/CyberPatriotWindows/Program.cs
@@ -2,11 +2,15 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+
 using Microsoft.Extensions.Configuration;
+
+using Newtonsoft.Json;
 
 public class Program
 {
   public static IConfiguration Configuration { get; set; }
+  public static string Config = "";
 
   public static void Main(string[] args = null)
   {
@@ -14,6 +18,8 @@ public class Program
       .AddCommandLine(args)
       .Build();
 
+    Config = builder["BuildFor"] + ".json";
+    
     var config = new ConfigurationBuilder()
       .SetBasePath(Directory.GetCurrentDirectory())
       .AddJsonFile(builder["BuildFor"] + ".json", optional: false, reloadOnChange: true)
@@ -27,5 +33,12 @@ public class Program
     Console.WriteLine();
 
     Console.WriteLine("Running checks");
-    }
+    
+    Console.WriteLine($"Checks: {Configuration["Checks:1:type"]}");
+  }
+
+  private static void RunCheckWindows(string type, string script)
+  {
+    
+  }
 }
