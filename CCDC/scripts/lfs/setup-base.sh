2e4ad252a05963b99b2b0f12b6efc512ee5d4190
Bailey Kasin <baileykasin@gmail.com>
2018-03-20 02:22:57 -0700

    Mostly work on LFS. Apparently the Arch live CD does not have make or gcc

--- CCDC/scripts/lfs/setup-base.sh
+++ CCDC/scripts/lfs/setup-base.sh
@@ -18,8 +18,6 @@ LANGUAGE='en_US.UTF-8'
 PASSWORD=$(/usr/bin/openssl passwd -crypt 'password')
 TIMEZONE='UTC'
 
-CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
-ROOT_PARTITION="${DISK}1"
 TARGET_DIR='/mnt'
 COUNTRY=${COUNTRY:-US}
 MIRRORLIST="https://www.archlinux.org/mirrorlist/?country=${COUNTRY}&protocol=http&protocol=https&ip_version=4&use_mirror_status=on"
@@ -74,6 +72,7 @@ function make_directories
 
   chown -v administrator $LFS/tools
   chown -v administrator $LFS/sources
+  chown -v administrator $LFS/boot
 }
 
 function partition_disk
@@ -109,39 +108,37 @@ function partition_disk
 partition_disk
 make_directories
 
+# Pause for a bit to let everything settle
+echo "Sleeping"
+sleep 20
 mount -v -t ext4 ${DISK}4 $LFS
 #mount -v -t ext2 ${DISK}2 $LFS/boot
 
+pacman -Sy
+pacman -Rns --noconfirm vim vi openconnect openvpn nmap ntp arch-install-scripts nano vim-runtime
+pacman -Sc --noconfirm
+pacman -S --noconfirm gcc make
+
+# Move into the main disk and download all the packages that will be needed
 cd $LFS
 wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
-wget http://www.linuxfromscratch.org/lfs/view/stables/md5sums --directory-prefix=$LFS/sources
 
-pushd $LFS/sources
-md5sum -c md5sums
-popd
-
-# Set administrators bash_profile
-cat > ~/.bash_profile << "EOF"
-exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
-EOF
-
-cat > ~/.bashrc << "EOF"
-set +h
+#exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 umask 022
 LFS=/mnt/lfs
+echo $LFS
 LC_ALL=POSIX
+echo $LC_ALL
 LFS_TGT=$(uname -m)-lfs-linux-gnu
-PATH=/tools/bin:/usr/bin
-export LFS LC_ALL LFS_TGT PATH
-EOF
-
-source ~/.bash_profile
+echo "On $LFS_TGT"
+PATH=/tools/bin:/bin:/usr/bin
 
 function build_binutils
 {
   cd $LFS/sources
-  tar xvf binutils*
+  tar xvf binutils-2.30.tar.xz
+  cd binutils-2.30
 
   mkdir -v build
   cd build
@@ -162,22 +159,22 @@ function build_binutils
   make install
 
   cd $LFS/sources
-  rm -rf binutils*
+  rm -rf binutils-2.30
 }
 
 function build_gcc
 {
   cd $LFS/sources
 
-  tar xvf gcc-*
-  cd gcc*
+  tar xvf gcc-7.3.0.tar.xz
+  cd gcc7.3.0
 
-  tar xvf ../mpfr-*
-  mv -v mpfr-* mpfr
-  tar xvf ../gmp-*
-  mv -v gmp-* gmp
-  tar xvf ../mpc-*
-  mv -v mpc-* mpc
+  tar xvf ../mpfr-4.0.1.tar.xz
+  mv -v mpfr-4.0.1 mpfr
+  tar xvf ../gmp-6.1.2.tar.xz
+  mv -v gmp-6.1.2 gmp
+  tar xvf ../mpc-1.1.0.tar.gz
+  mv -v mpc-1.1.0 mpc
 
   for file in gcc/config/{linux,i386/linux{,64}}.h
   do
@@ -227,14 +224,17 @@ function build_gcc
 
   make
   make install
+
+  cd $LFS/sources
+  rm -rf gcc-7.3.0
 }
 
 function build_linux_headers
 {
   cd $LFS/sources
-  tar xvf linux-*
+  tar xvf linux-4.15.3.tar.xz
+  cd linux-4.15.3
 
-  cd linux-*
   make mrproper
 
   make INSTALL_HDR_PATH=dest headers_install
@@ -244,9 +244,9 @@ function build_linux_headers
 function build_glibc
 {
   cd $LFS/sources
-  tar xvf glibc-*
+  tar xvf glibc-2.27.tar.xz
 
-  cd glibc-*
+  cd glibc-2.27
   mkdir -v build
   cd build
 
@@ -266,9 +266,9 @@ function build_glibc
 function build_libstdc
 {
   cd $LFS/sources
-  tar xvf gcc-*
+  tar xvf gcc-7.3.0.tar.xz
+  cd gcc-7.3.0
 
-  cd gcc-*
   mkdir -v build
   cd build
 
@@ -283,14 +283,17 @@ function build_libstdc
   
   make
   make install
+
+  $LFS/sources
+  rm -rf gcc-7.3.0
 }
 
 function build_binutils_again
 {
   cd $LFS/sources
-  tar xvf binutils-*
+  tar xvf binutils-2.30.tar.xz
+  cd binutils-2.30
 
-  cd binutils-*
   mkdir -v build
   cd build
 
@@ -315,9 +318,9 @@ function build_binutils_again
 function build_gcc_again
 {
   cd $LFS/sources
-  tar xvf gcc-*
+  tar xvf gcc-7.3.0.tar.xz
+  cd gcc-7.3.0
 
-  cd gcc-*
   cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
     `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
 
@@ -341,12 +344,12 @@ function build_gcc_again
     ;;
   esac
 
-  tar -xf ../mpfr-*
-  mv -v mpfr-* mpfr
-  tar -xf ../gmp-*
-  mv -v gmp-* gmp
-  tar -xf ../mpc-*
-  mv -v mpc-* mpc
+  tar xvf ../mpfr-4.0.1.tar.xz
+  mv -v mpfr-4.0.1 mpfr
+  tar xvf ../gmp-6.1.2.tar.xz
+  mv -v gmp-6.1.2 gmp
+  tar xvf ../mpc-1.1.0.tar.gz
+  mv -v mpc-1.1.0 mpc
 
   mkdir -v build
   cd build
@@ -374,8 +377,8 @@ function build_gcc_again
 function build_tcl
 {
   cd $LFS/sources
-  tar xvf tcl*
-  cd tcl*
+  tar xvf tcl8.6.8-src.tar.gz
+  cd tcl8.6.8-src
 
   cd unix
   ./configure --prefix=/tools
@@ -394,8 +397,8 @@ function build_tcl
 function build_expect
 {
   cd $LFS/sources
-  tar xvf expect*
-  cd expect*
+  tar xvf expect5.45.4.tar.gz
+  cd expect5.45.4
 
   cp -v configure{,orig}
   sed 's:/usr/local/bin:/bin:' configure.orig > configure
@@ -411,8 +414,8 @@ function build_expect
 function build_dejagnu
 {
   cd $LFS/sources
-  tar xvf dejagnu-*
-  cd dejagnu-*
+  tar xvf dejagnu-1.6.1.tar.gz
+  cd dejagnu-1.6.1
 
   ./configure --prefix=/tools
 
@@ -422,8 +425,8 @@ function build_dejagnu
 function build_m4
 {
   cd $LFS/sources
-  tar xvf m4-*
-  cd m4-*
+  tar xvf m4-1.4.18.tar.xz
+  cd m4-1.4.18
 
   ./configure --prefix=/tools
   make
@@ -433,8 +436,8 @@ function build_m4
 function build_ncurses
 {
   cd $LFS/sources
-  tar xvf ncurses-*
-  cd ncurses-*
+  tar xvf ncurses-6.1.tar.gz
+  cd ncurses-6.1
 
   sed -i s/mawk// configure
   ./configure --prefix=/tools \
@@ -451,8 +454,8 @@ function build_ncurses
 function build_bash
 {
   cd $LFS/sources
-  tar xvf bash-*
-  cd bash-*
+  tar xvf bash-4.4.18.tar.gz
+  cd bash-4.4.18
 
   ./configure --prefix=/tools \
               --without-bash-malloc
@@ -466,8 +469,8 @@ function build_bash
 function build_bison
 {
   cd $LFS/sources
-  tar xvf bison-*
-  cd bison-*
+  tar xvf bison-3.0.4.tar.xz
+  cd bison-3.0.4
 
   ./configure --prefix=/tools
 
@@ -478,8 +481,8 @@ function build_bison
 function build_bzip
 {
   cd $LFS/sources
-  tar xvf bzip*
-  cd bzip*
+  tar xvf bzip2-1.0.6.tar.gz
+  cd bzip2-1.0.6
   
   make
   make PREFIX=/tools install
@@ -488,8 +491,8 @@ function build_bzip
 function build_coreutils
 {
   cd $LFS/sources
-  tar xvf coreutils-*
-  cd coreutils-*
+  tar xvf coreutils-8.29.tar.xz
+  cd coreutils-8.29
 
   ./configure --prefix=/tools --enable-install-program=hostname
 
@@ -500,8 +503,8 @@ function build_coreutils
 function build_diffutils
 {
   cd $LFS/sources
-  tar xvf diffutils-*
-  cd diffutils-*
+  tar xvf diffutils-3.6.tar.xz
+  cd diffutils-3.6
 
   ./configure --prefix=/tools
   make
@@ -511,8 +514,8 @@ function build_diffutils
 function build_file
 {
   cd $FLS/sources
-  tar xvf file-*
-  cd file-*
+  tar xvf file-5.32.tar.gz
+  cd file-5.32
 
   ./configure --prefix=/tools
   make
@@ -522,8 +525,8 @@ function build_file
 function build_findutils
 {
   cd $LFS/sources
-  tar xvf findutils-*
-  cd findutils-*
+  tar xvf findutils-4.6.0.tar.gz
+  cd findutils-4.6.0
 
   ./configure --prefix=/tools
   make
@@ -533,8 +536,8 @@ function build_findutils
 function build_gawk
 {
   cd $LFS/sources
-  tar xvf gawk-*
-  cd gawk-*
+  tar xvf gawk-4.2.0.tar.xz
+  cd gawk-4.2.0
 
   ./configure --prefix=/tools
   make
@@ -544,8 +547,8 @@ function build_gawk
 function build_gettext
 {
   cd $LFS/sources
-  tar xvf gettext-*
-  cd gettext-*
+  tar xvf gettext-0.19.8.1.tar.xz
+  cd gettext-0.19.8.1
 
   cd gettext-tools
   EMACS="no" ./configure --prefix=/tools --disable-shared
@@ -562,8 +565,8 @@ function build_gettext
 function build_grep
 {
   cd $LFS/sources
-  tar xvf grep-*
-  cd grep-*
+  tar xvf grep-3.1.tar.xz
+  cd grep-3.1
 
   ./configure --prefix=/tools
   make
@@ -573,8 +576,8 @@ function build_grep
 function build_gzip
 {
   cd $LFS/sources
-  tar xvf gzip-*
-  cd gzip-*
+  tar xvf gzip-1.9.tar.xz
+  cd gzip-1.9
 
   ./configure --prefix=/tools
   make
@@ -584,8 +587,8 @@ function build_gzip
 function build_make
 {
   cd $LFS/sources
-  tar xvf make-*
-  cd make-*
+  tar xvf make-4.2.1.tar.bz2
+  cd make-4.2.1
 
   sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c
   ./configure --prefix=/tools \
@@ -598,8 +601,8 @@ function build_make
 function build_patch
 {
   cd $LFS/sources
-  tar xvf patch-*
-  cd patch-*
+  tar xvf patch-2.7.6.tar.xz
+  cd patch-2.7.6
 
   ./configure --prefix=/tools
   make
@@ -609,8 +612,8 @@ function build_patch
 function build_perl
 {
   cd $LFS/sources
-  tar xvf perl-*
-  cd perl-*
+  tar xvf perl-5.26.1.tar.xz
+  cd perl-5.26.1
 
   sh Configure -des -Dprefix=/tools -Dlibs=-lm
   make
@@ -623,8 +626,8 @@ function build_perl
 function build_sed
 {
   cd $FLS/sources
-  tar xvf sed-*
-  cd sed-*
+  tar xvf sed-4.4.tar.xz
+  cd sed-4.4
 
   ./configure --prefix=/tools
   make
@@ -634,8 +637,8 @@ function build_sed
 function build_tar
 {
   cd $LFS/sources
-  tar xvf tar-*
-  cd tar-*
+  tar xvf tar-1.30.tar.xz
+  cd tar-1.30
 
   ./configure --prefix=/tools
   make
@@ -645,8 +648,8 @@ function build_tar
 function build_texinfo
 {
   cd $LFS/sources
-  tar xvf texinfo-*
-  cd texinfo-*
+  tar xvf texinfo-6.5.tar.xz
+  cd texinfo-6.5
 
   ./configure --prefix=/tools
   make
@@ -656,8 +659,8 @@ function build_texinfo
 function build_util_linux
 {
   cd $LFS/sources
-  tar xvf util-linux-*
-  cd util-linux-*
+  tar xvf util-linux-2.31.1.tar.xz
+  cd util-linux-2.31.1
 
   ./configure --prefix=/tools                \
               --without-python               \
@@ -673,8 +676,8 @@ function build_util_linux
 function build_xz
 {
   cd $LFS/sources
-  tar xvf xz-*
-  cd xz-*
+  tar xvf xz-5.2.3.tar.xz
+  cd xz-5.2.3
 
   ./configure --prefix=/tools
   make
@@ -686,20 +689,8 @@ build_gcc
 build_linux_headers
 build_glibc
 
-GCC_WGET_VER=$(cat $LFS/wget-list | grep gcc)
-rm -rf $LFS/sources/gcc-*
-
-wget $GCC_WGET_VER --directory-prefix=$LFS/sources
 build_libstdc
-
-BINUTILS_WGET_VER=$(cat $LFS/wget-list | grep binutils)
-rm -rf $LFS/sources/binutils-*
-
-wget $BINUTILS_WGET_VER --directory-prefix=$LFS/sources
 build_binutils_again
-
-rm -rf $LFS/sources/gcc-*
-wget $GCC_WGET_VER --directory-prefix=$LFS/sources
 build_gcc_again
 
 build_tcl
